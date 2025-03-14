28fb45356f36289637b971af75cb373e855df138
AnastaZIuk <areklachowicz@gmail.com>
2025-03-14 16:09:51 +0100

    finally it works

---
+++ Dockerfile
@@ -0,0 +1,61 @@
+# escape=`
+
+FROM --platform=windows/amd64 mcr.microsoft.com/windows/servercore:ltsc2025 AS BUILD_TOOLS
+
+ARG WINDOWS_11_SDK_VERSION="22621"
+ARG VC_VERSION="14.42.17.12"
+ARG MSVC_VERSION="14.42.34433"
+
+ENV WINDOWS_11_SDK_VERSION=${WINDOWS_11_SDK_VERSION} VC_VERSION=${VC_VERSION} MSVC_VERSION=${MSVC_VERSION} BUILD_TOOLS_DIR="C:\artifacts"
+
+RUN mkdir C:\Temp && cd C:\Temp `
+&& curl -SL --output vs_buildtools.exe https://aka.ms/vs/17/release/vs_buildtools.exe `
+&& (start /w vs_buildtools.exe --quiet --wait --norestart --nocache `
+--remove Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
+--add Microsoft.VisualStudio.Component.VC.%VC_VERSION%.x86.x64 `
+--add Microsoft.VisualStudio.Component.Windows11SDK.%WINDOWS_11_SDK_VERSION% `
+--installPath %BUILD_TOOLS_DIR% `
+|| IF "%ERRORLEVEL%"=="3010" EXIT 0) `
+&& dir %BUILD_TOOLS_DIR%\VC\Tools\MSVC `
+&& if exist %BUILD_TOOLS_DIR%\VC\Tools\MSVC\%MSVC_VERSION% ( `
+for /d %i in (%BUILD_TOOLS_DIR%\VC\Tools\MSVC\*) do if /I not "%i"=="%BUILD_TOOLS_DIR%\VC\Tools\MSVC\%MSVC_VERSION%" rd /s /q "%i" `
+) else ( `
+echo "Error: Expected MSVC version directory %MSVC_VERSION% does not exist!" && exit /b 1 `
+)
+
+RUN (echo { "WINDOWS_SDK_VERSION": "10.0.%WINDOWS_11_SDK_VERSION%.0", "VC_VERSION": "%VC_VERSION%", "MSVC_VERSION": "%MSVC_VERSION%" }) > %BUILD_TOOLS_DIR%\env.json
+
+FROM mcr.microsoft.com/windows/nanoserver:ltsc2022
+
+ARG BUILD_TOOLS_DIR="C:\BuildTools"
+ARG WINDOWS_KITS_10_DIR="C:\WindowsKits10SDK"
+ARG CMAKE_VERSION=3.31.0
+ARG CMAKE_DIR="C:\CMake"
+ARG PYTHON_VERSION=3.11.0
+ARG PYTHON_DIR="C:\Python"
+ARG NINJA_VERSION=1.12.1
+ARG NINJA_DIR="C:\Ninja"
+ARG NASM_VERSION=2.16.03
+ARG NASM_DIR="C:\Nasm"
+
+COPY --link --from=BUILD_TOOLS ["C:/Program Files (x86)/Windows Kits/10", "${WINDOWS_KITS_10_DIR}"]
+COPY --link --from=BUILD_TOOLS ["C:/artifacts", "${BUILD_TOOLS_DIR}"]
+
+ENV CMAKE_WINDOWS_KITS_10_DIR=${WINDOWS_KITS_10_DIR} BUILD_TOOLS_DIR=${BUILD_TOOLS_DIR} `
+CMAKE_VERSION=${CMAKE_VERSION} CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-windows-x86_64.zip CMAKE_DIR=${CMAKE_DIR} `
+PYTHON_VERSION=${PYTHON_VERSION} PYTHON_URL=https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-embed-amd64.zip PYTHON_DIR=${PYTHON_DIR} `
+NINJA_VERSION=${NINJA_VERSION} NINJA_URL=https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-win.zip NINJA_DIR=${NINJA_DIR} `
+NASM_VERSION=${NASM_VERSION} NASM_URL=https://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/win64/nasm-${NASM_VERSION}-win64.zip NASM_DIR=${NASM_DIR}
+
+RUN mkdir "%CMAKE_DIR%" && curl -SL --output cmake.zip %CMAKE_URL% && tar -xf cmake.zip -C "%CMAKE_DIR%" && del cmake.zip
+RUN mkdir "%PYTHON_DIR%" && curl -SL --output python.zip %PYTHON_URL% && tar -xf python.zip -C "%PYTHON_DIR%" && del python.zip
+RUN mkdir "%NINJA_DIR%" && curl -SL --output ninja.zip %NINJA_URL% && tar -xf ninja.zip -C "%NINJA_DIR%" && del ninja.zip
+RUN mkdir "%NASM_DIR%" && curl -SL --output nasm.zip %NASM_URL% && tar -xf nasm.zip -C "%NASM_DIR%" && del nasm.zip
+
+RUN cd %BUILD_TOOLS_DIR% && "%PYTHON_DIR%\python.exe" -c "import json, os; env=json.load(open('./env.json')); [os.system(f'setx {k} \"{v}\"') for k,v in env.items()]" `
+&& setx PATH "%CMAKE_DIR%\cmake-%CMAKE_VERSION%-windows-x86_64\bin;%PYTHON_DIR%;%NINJA_DIR%;%NASM_DIR%\nasm-%NASM_VERSION%;%PATH%" `
+&& setx MSVC_TOOLSET_DIR "%BUILD_TOOLS_DIR%\VC\Tools\MSVC\%MSVC_VERSION%"
+
+SHELL ["cmd", "/c"]
+ENTRYPOINT ["cmd"]
+CMD ["/noexit"]
--- cmake/winsdk-msvc-toolchain.cmake
+++ cmake/winsdk-msvc-toolchain.cmake
@@ -1,27 +1,44 @@
 message(STATUS "Configuring with \"${CMAKE_CURRENT_LIST_FILE}\" toolchain")
 
+if(CMAKE_VERSION VERSION_LESS "3.31.0")
+    message(FATAL_ERROR "CMake version must be at least 3.31.0 to use this toolchain file!")
+endif()
+
 if(NOT CMAKE_GENERATOR MATCHES "Ninja*")
     message(FATAL_ERROR "CMAKE_GENERATOR = \"${CMAKE_GENERATOR}\" is unsupported, use Ninja (single or multi config) generators!")
 endif()
 
-cmake_path(CONVERT $ENV{WINDOWS_SDK_VERSION} TO_CMAKE_PATH_LIST WINDOWS_SDK_VERSION NORMALIZE)
 cmake_path(CONVERT $ENV{CMAKE_WINDOWS_KITS_10_DIR} TO_CMAKE_PATH_LIST WINDOWS_KITS_10_DIR NORMALIZE)
+cmake_path(CONVERT $ENV{WINDOWS_SDK_VERSION} TO_CMAKE_PATH_LIST WINDOWS_SDK_VERSION NORMALIZE)
 cmake_path(CONVERT $ENV{MSVC_TOOLSET_DIR} TO_CMAKE_PATH_LIST MSVC_TOOLSET_DIR NORMALIZE)
 
-message(STATUS "WINDOWS_KITS_10_DIR = \"${WINDOWS_KITS_10_DIR}\"")
-message(STATUS "WINDOWS_SDK_VERSION = \"${WINDOWS_SDK_VERSION}\"")
-message(STATUS "MSVC_TOOLSET_DIR = \"${MSVC_TOOLSET_DIR}\"")
+if(VERBOSE)
+    message(STATUS "WINDOWS_KITS_10_DIR = \"${WINDOWS_KITS_10_DIR}\"")
+    message(STATUS "WINDOWS_SDK_VERSION = \"${WINDOWS_SDK_VERSION}\"")
+    message(STATUS "MSVC_TOOLSET_DIR = \"${MSVC_TOOLSET_DIR}\"")
+endif()
 
 set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
 
-# TODO: one could allow for cross platform builds 
-# and more archs but I hardcode temporary windows-x86_64
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+if(ARCH)
+    if(ARCH MATCHES "x64")
+    elseif(ARCH MATCHES "arm64")
+        set(CMAKE_SYSTEM_PROCESSOR arm64)
+        # never tested it but I'm pretty sure _COMPILER needs to be updated depending on target ARCH
+        message(FATAL_ERROR "TODO, Cross compilation requires ARM64 build tools compoment and updates to the toolchain file!")
+    else()
+        message(STATUS "Unsupported ARCH = \"${ARCH}\"")
+        message(FATAL_ERROR "Supported ARCH = \"x64\", \"arm64\"")
+    endif()
+else()
+ set(ARCH x64)
+endif()
 
 set(LIB
-    "${WINDOWS_KITS_10_DIR}/Lib/${WINDOWS_SDK_VERSION}/ucrt/x64"
-    "${WINDOWS_KITS_10_DIR}/Lib/${WINDOWS_SDK_VERSION}/um/x64"
-    "${MSVC_TOOLSET_DIR}/lib/x64"
+    "${WINDOWS_KITS_10_DIR}/Lib/${WINDOWS_SDK_VERSION}/ucrt/${ARCH}"
+    "${WINDOWS_KITS_10_DIR}/Lib/${WINDOWS_SDK_VERSION}/um/${ARCH}"
+    "${MSVC_TOOLSET_DIR}/lib/${ARCH}"
 )
 
 set(INCLUDE
@@ -48,15 +65,15 @@ endfunction()
 _VALIDATE_ENV_V_(LIB)
 _VALIDATE_ENV_V_(INCLUDE)
 
-set(CMAKE_C_COMPILER "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/cl.exe")
-set(CMAKE_CXX_COMPILER "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/cl.exe")
-set(CMAKE_ASM_COMPILER "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/cl.exe")
-set(CMAKE_ASM_MASM_COMPILER "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/ml64.exe")
+set(CMAKE_C_COMPILER "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/cl.exe")
+set(CMAKE_CXX_COMPILER "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/cl.exe")
+set(CMAKE_ASM_COMPILER "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/cl.exe")
+set(CMAKE_ASM_MASM_COMPILER "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/ml64.exe")
 find_program(CMAKE_ASM_NASM_COMPILER nasm HINTS ENV PATH ENV NASM_DIR NO_CACHE REQUIRED)
-set(CMAKE_RC_COMPILER "${WINDOWS_KITS_10_DIR}/bin/${WINDOWS_SDK_VERSION}/x64/rc.exe")
-set(CMAKE_LINKER "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/link.exe")
-set(CMAKE_AR "${MSVC_TOOLSET_DIR}/bin/Hostx64/x64/lib.exe")
-set(CMAKE_MT "${WINDOWS_KITS_10_DIR}/bin/${WINDOWS_SDK_VERSION}/x64/mt.exe")
+set(CMAKE_RC_COMPILER "${WINDOWS_KITS_10_DIR}/bin/${WINDOWS_SDK_VERSION}/${ARCH}/rc.exe")
+set(CMAKE_LINKER "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/link.exe")
+set(CMAKE_AR "${MSVC_TOOLSET_DIR}/bin/Host${ARCH}/${ARCH}/lib.exe")
+set(CMAKE_MT "${WINDOWS_KITS_10_DIR}/bin/${WINDOWS_SDK_VERSION}/${ARCH}/mt.exe")
 
 macro(_UPDATE_STANDARD_DIRECTORIES_ WHAT VARV)
     set(CMAKE_C_STANDARD_${WHAT}_DIRECTORIES ${VARV})
