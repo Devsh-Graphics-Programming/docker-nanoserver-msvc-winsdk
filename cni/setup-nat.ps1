33078f279a4b3d1042687ce7bac2a717319bf74e
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2025-03-18 12:31:39 +0100

    Update setup-nat.ps1

--- cni/setup-nat.ps1
+++ cni/setup-nat.ps1
@@ -1,9 +1,6 @@
+# https://github.com/moby/buildkit/blob/248ff7c29ed979ef0c142b8166dd6d192cf0b215/docs/windows.md#cni--networking-setup
 $workspaceDir = Resolve-Path "$PSScriptRoot\.."
 $networkName = 'nat'
-# Get-HnsNetwork is available once you have enabled the 'Hyper-V Host Compute Service' feature
-# which must have been done at the Quick setup above
-# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V, Containers -All
-# the default one named `nat` should be available, except for WS2019, see notes below.
 $natInfo = Get-HnsNetwork -ErrorAction Ignore | Where-Object { $_.Name -eq $networkName }
 if ($null -eq $natInfo) {
     throw "NAT network not found, check if you enabled containers, Hyper-V features and restarted the machine"
@@ -11,15 +8,14 @@ if ($null -eq $natInfo) {
 $gateway = $natInfo.Subnets[0].GatewayAddress
 $subnet = $natInfo.Subnets[0].AddressPrefix
 
-$cniConfPath = "$workspaceDir\buildkit\containerd\cni\conf\0-containerd-nat.conf"
-$cniBinDir = "$workspaceDir\buildkit\cni\bin"
 $cniVersion = "1.0.0"
 $cniPluginVersion = "0.3.1"
+$cniBinDir = "$workspaceDir\buildkit\cni"
+$cniConfPath = "$cniBinDir\0-containerd-nat.conf"
 
-# get the CNI plugins (binaries)
 mkdir $cniBinDir -Force
 curl.exe -LO https://github.com/microsoft/windows-container-networking/releases/download/v$cniPluginVersion/windows-container-networking-cni-amd64-v$cniPluginVersion.zip
-tar xvf windows-container-networking-cni-amd64-v$cniPluginVersion.zip -C $cniBinDir
+Expand-Archive -Path windows-container-networking-cni-amd64-v$cniPluginVersion.zip -DestinationPath $cniBinDir -Force
 
 $natConfig = @"
 {
@@ -42,5 +38,4 @@ $natConfig = @"
 }
 "@
 Set-Content -Path $cniConfPath -Value $natConfig
-# take a look
 cat $cniConfPath
